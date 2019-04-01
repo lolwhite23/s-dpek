68e9668cc90d9bb884ff6e7f1ce1f65595154a59
lolwhite23 <48812604+lolwhite23@users.noreply.github.com>
2019-04-01 09:33:20 -0400

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,10 @@
+module.exports.run = async(bot, message, args) => {
+  message.channel.send("This bot was taken over, no commands work, good luck :)");
+
+
+
+}
+module.exports.help = {
+  name: 'help'
+
+}
