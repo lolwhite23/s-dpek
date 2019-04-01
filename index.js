607d74f5fa00e6c61a5cadbdf4929c90521ab196
lolwhite23 <48812604+lolwhite23@users.noreply.github.com>
2019-04-01 09:23:58 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,200 @@
+const discord = require('discord.js');
+const message = require('./handlers/message');
+const utils = require('./global/utils');
+const bot = new discord.Client();
+const client = new discord.Client();
+const fs = require("fs");
+const ms = require("ms");
+const botconfig = require('./settiings/config.json')
+const db = require('quick.db');
+
+require('./global/functions')(bot, utils, botconfig);
+
+bot.commands = new discord.Collection();
+bot.aliases = new discord.Collection();
+message.message(bot, utils, botconfig, discord);
+
+
+
+
+
+bot.on('ready', function() {
+
+  console.log(`${bot.user.username} is online!`)
+  setInterval(async () => {
+const statuslist = [
+  "play.zeptixmc.net | v1.2",
+  `${bot.channels.size} channels | ~help`,
+  `${bot.users.size} users | v1.2`
+];
+const random = Math.floor(Math.random() * statuslist.length);
+
+try {
+  await bot.user.setPresence({
+    game: {
+      name: `${statuslist[random]}`,
+      type: "WATCHING"
+      //url: 'https://www.twitch.tv/spokloo'
+    },
+    status: "online"
+  });
+} catch (error) {
+  console.error(error);
+}
+}, 10000);
+});
+
+bot.on('guildMemberAdd', member => {
+  member.guild.channels.get('442504610066857985').setName(`Total Users: ${member.guild.memberCount}`);
+  if(member.bot) return;
+    let role = member.guild.roles.find("name", "Members");
+    member.addRole(role).catch(console.error);
+  let welome = new discord.RichEmbed()
+  .setAuthor('Welcome!', bot.user.displayAvatarURL)
+  .setThumbnail(member.user.displayAvatarURL)
+  .addField(`Welcome ${member.user.tag} to the server!`, '_ _')
+  .addField("Total members:", `${member.guild.memberCount}`)
+  .setColor("#2CDD19")
+  .setFooter("Covalia | Logs")
+  .setTimestamp()
+ let welcomeChannel = member.guild.channels.find('name', 'welcome');
+  welcomeChannel.send(welome)
+
+  let logChannel = member.guild.channels.find('name', 'mod-log');
+
+    let logEmbed1 = new discord.RichEmbed()
+    .setAuthor("Member joined", bot.user.displayAvatarURL)
+    .setThumbnail(member.user.displayAvatarURL)
+    .addField(member.user.username + " Has joined the server.", '_ _')
+    .addField(`ID: ${member.user.id}`, '_ _')
+    .addField(`Tag: #${member.user.discriminator}`, '_ _')
+    .addField("Total members:", `${member.guild.memberCount}`)
+    .setColor("#2CDD19")
+    .setFooter("Covalia | Logs")
+    .setTimestamp()
+    logChannel.send(logEmbed1);
+  })
+
+  bot.on('guildMemberRemove', member => {
+    
+    member.guild.channels.get('442504610066857985').setName(`Total Users: ${member.guild.memberCount}`);
+    
+      let leave = new discord.RichEmbed()
+  .setAuthor('Goodbye!', bot.user.displayAvatarURL)
+  .setThumbnail(member.user.displayAvatarURL)
+  .addField(`${member.user.tag} has left the server, we will miss you!`, '_ _')
+  .addField("Total members:", `${member.guild.memberCount}`)
+  .setColor("#DD1919")
+  .setFooter("Covalia | Logs")
+  .setTimestamp()
+    
+    let leaveChannel = member.guild.channels.find('name', 'welcome');
+          
+    leaveChannel.send(leave);
+    
+    
+  let logChannel = member.guild.channels.find('name', 'mod-log');
+
+    let logEmbed2 = new discord.RichEmbed()
+    .setAuthor("Member left", bot.user.displayAvatarURL)
+    .setThumbnail(member.user.displayAvatarURL)
+      .addField(member.user.username + " Has left the server.", '_ _')
+      .addField(`ID: ${member.user.id}`, '_ _')
+      .addField(`Tag: #${member.user.discriminator}`, '_ _')
+      .addField("Total members:", `${member.guild.memberCount}`)
+    .setFooter("Covalia | Logs")
+    .setColor("#DD1919")
+    .setTimestamp()
+    logChannel.send(logEmbed2);
+  })
+
+let y = process.openStdin()
+y.addListener("data", res => {
+  let x = res.toString().trim().split(/ +/g)
+  bot.channels.get("558116244822163487").send(x.join(" "));
+})
+
+bot.on('message', async message => {
+
+  // parseTime fu
+
+let status = new db.table('AFKs');
+
+// Check if author is AFK
+let authorStatus = await status.fetch(message.author.id);
+
+let entries = await db.fetch('afkreason');
+let name = await db.fetch('afkreason')
+
+
+if (entries instanceof Array) entries = entries.slice(-1);
+
+
+if (authorStatus) { // Run if they are AFK
+
+  const afkembed = new discord.RichEmbed()
+    .setColor("#36393e")
+    .setDescription(`${message.author.username} is no longer AFK.`)
+	
+  // Send a 'You are no longer AFK message'
+  message.channel.send(afkembed).then(msg => msg.delete(5000));
+  
+  // This will delete the user from the AFK pool in the database
+  status.delete(message.author.id);
+
+}
+
+let mentioned = message.mentions.members.first(); // This will store the first member mentioned
+if (mentioned) { // This will run if a member is mentioned
+
+  // Access AFK Status
+  let statusw = await status.fetch(mentioned.id);
+  let changelog = '';
+  for (var i in entries.reverse()) {
+    changelog += `**${entries[i].entry}**\n`
+  }
+  let changelog2 = '';
+  for (var i in entries.reverse()) {
+    changelog2 += `${entries[i].name}\n`
+  }
+  if (statusw) { // This will run if they are AFK (since the db will return true)
+
+    message.delete();
+    const embed = new discord.RichEmbed()
+      .setColor("#36393e")
+      .setAuthor(`${changelog2} is AFK!`, bot.user.displayAvatarURL)
+      .addField(`Reason: ${changelog}`, '_ _')
+
+    // Send Embed
+    message.channel.send(embed);
+
+  }
+
+
+}
+bot.on('messageDelete', async (message) => {
+	
+
+    const logs = message.guild.channels.find('name', 'mod-log');
+    const entry = await message.guild.fetchAuditLogs({
+        type: 'MESSAGE_DELETE'
+    }).then(audit => audit.entries.first())
+    let user;
+    if (entry.extra.channel.id === message.channel.id && (entry.target.id === message.author.id) && (entry.createdTimestamp > (Date.now() - 5000)) && (entry.extra.count >= 1)) {
+        user = entry.executor.username
+    } else {
+        user = message.author
+    }
+    const logembed = new discord.RichEmbed()
+        //.setTitle('Message Deleted')
+        .setAuthor(message.author.tag, message.author.displayAvatarURL)
+        .addField(`**Message sent by ${message.author.username} deleted in #${message.channel.name}**\n\n`, message.content)
+        .setColor()
+        .setFooter(`<#${message.channel.id}>`)
+        .setTimestamp()
+    //console.log(entry)
+    logs.send(logembed);;
+})
+});
+
+bot.login(process.env.token)
